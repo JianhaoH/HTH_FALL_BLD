18a5f4911952f70c606aa18b948b6dd1047e30f5
JianhaoH <111212933+JianhaoH@users.noreply.github.com>
2022-10-06 18:52:49 -0700

    as

---
+++ about_me.py
@@ -0,0 +1,11 @@
+name = 'jianhao he'
+prefname = 'jianhao'
+age = 17
+usedPython = True
+hobbies = ['gaming','gym']
+favThings = {
+  'movie': 'Top gun Maverick',
+  'food': 'rice',
+  'hobby': 'game',
+}
+all_vars = dict(vars())
