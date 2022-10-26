f39c86156fa3126904dc09e3ae48716f71645107
JianhaoH <111212933+JianhaoH@users.noreply.github.com>
2022-10-25 19:00:54 -0700

    a

--- WK3_BLD/main.py
+++ WK3_BLD/main.py
@@ -4,3 +4,6 @@ pandas.options.display.max_columns = None
 pandas.options.display.max_rows = None
 
 csv_data = pandas.read_csv("Pokemon.csv")
+csv_data
+'''which pokemon has the most defense?'''
+csv_data.sort_values("Defense", ascending=False)
