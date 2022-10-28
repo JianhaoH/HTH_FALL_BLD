bc160a9dc28c9aed9211b5a8bbfbe120086fdae0
JianhaoH <111212933+JianhaoH@users.noreply.github.com>
2022-10-27 17:38:33 -0700

    a

--- PandaDataExploration/main.py
+++ PandaDataExploration/main.py
@@ -5,5 +5,9 @@ pandas.options.display.max_rows = None
 
 csv_data = pandas.read_csv("Pokemon.csv")
 csv_data
-'''which pokemon has the most defense?'''
-csv_data.sort_values("Defense", ascending=False)
+'''which type of pokemon has the highest ATK'''
+csv_data.groupby("Type 1")["Attack"].mean().sort_values(ascending=False)
+'''which dragon pokemons have the most attack from highest to lowest'''
+csv_data[csv_data["Type 1"] == "Dragon"].sort_values(by="Attack", ascending=False)
+'''which generation has the most legedenary pokemon'''
+csv_data[csv_data["Legendary"] == True].groupby("Generation")["Legendary"].count().sort_values(ascending=False)
